0c262d9a0926dec830db3af662957a680a52a233
MohdSajid8010 <117560964+MohdSajid8010@users.noreply.github.com>
2023-05-04 16:00:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function myFunc()
+	{
+		let promise =new Promise((resolve)=>{
+			setTimeout(()=>{
+				resolve("Hello, world!");
+			},1000);
+		})
+	}
+
+let promise = myFunc();
+promise.then((data)=>{
+	document.getElementById("output").innerHTML= data;
+}).catch((err)=>{
+	console.log(err);
+})
